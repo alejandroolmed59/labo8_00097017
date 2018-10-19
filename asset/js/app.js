d21998a76394fd80b9dc893799632241b7b91fc3
Alejandro Olmedo <00097017@uca.edu.sv>
2018-10-19 11:33:40 -0600

    anniadidos ultimos cambios

--- asset/js/app.js
+++ asset/js/app.js
@@ -33,26 +33,65 @@ var formulario= document.getElementById("bitacora");
 formulario.addEventListener("submit", (evt) => {
     evt.preventDefault(); 
     let bitacora = { 
-        cant:cont, 
-        fecha: formulario[1].value, 
-        descripcion: formulario[2].value, 
-        cantidad: formulario[3].value 
+    cant:cont, 
+    fecha: formulario[1].value, 
+    descripcion: formulario[2].value, 
+    cantidad: formulario[3].value 
   } 
-  bitacoras.push(bitacora);
-  cont++;
-  mostrar();
+    if(comprobar()){
+        bitacoras.push(bitacora);
+        cont++;
+        mostrar();
+    }
  }); 
+
+ const comprobar = () =>{
+    var fecha= formulario[1].value;
+    var descp= formulario[2].value;
+    var cant= formulario[3].value;
+    if(fecha =="" || descp=="" || cant==""){
+        if(fecha==""){
+            formulario[1].style.border="4px solid red";
+        }else{
+            formulario[1].style.border="4px solid green";
+        }
+        if(descp==""){
+            formulario[2].style.border="4px solid red";
+        }else{
+            formulario[2].style.border="4px solid green";
+        }
+        if(cant==""){
+            formulario[3].style.border="4px solid red";
+        }else{
+            formulario[3].style.border="4px solid green";
+        }
+        return false;
+    }else{
+        if(fecha.length>0){
+            formulario[1].style.border="4px solid green";
+        }
+        if(descp.length>0){
+            formulario[2].style.border="4px solid green";
+        }
+        if(cant.length>0){
+            formulario[3].style.border="4px solid green";
+        }
+        return true;
+    }
+
+
+ }
 
  const crearElemento = (bitacora, tbody) =>{ 
     let tr = document.createElement("tr"); 
     Object.values(bitacora).forEach(item => { 
-        let td = document.createElement("td"); 
-        let content = document.createTextNode(item); 
-        td.appendChild(content); 
-        tr.setAttribute("class", "click"); 
-        tr.appendChild(td); 
+     let td = document.createElement("td"); 
+     let content = document.createTextNode(item); 
+     td.appendChild(content); 
+     tr.setAttribute("class", "click"); 
+     tr.appendChild(td); 
     }); 
-    tbody.appendChild(tr); 
+   tbody.appendChild(tr); 
   } 
 
 const eliminar= (tbody)=>{
@@ -62,16 +101,17 @@ const eliminar= (tbody)=>{
 }
  
 const agregar= ()=>{ 
- var eventtr = document.querySelectorAll(".click"); 
-   eventtr.forEach((item, index) => { 
-    item.addEventListener("click", () => { 
-    document.querySelector("#fecha").value = item.childNodes[1].textContent; 
-    document.querySelector("#descp").value = item.childNodes[2].textContent; 
-    document.querySelector("#cant").value = item.childNodes[3].textContent; 
-  }); 
- }) 
-} 
-const mostrar = ()=>{ 
+    var eventtr = document.querySelectorAll(".click"); 
+      eventtr.forEach((item, index) => { 
+      item.addEventListener("click", () => { 
+      document.querySelector("#fecha").value = item.childNodes[1].textContent; 
+      document.querySelector("#descp").value = item.childNodes[2].textContent; 
+      document.querySelector("#cant").value = item.childNodes[3].textContent; 
+     }); 
+    }) 
+   } 
+   
+   const mostrar = ()=>{ 
     if (document.querySelector(".tabla-btc tbody").childElementCount > 0) { 
      eliminar(document.querySelector(".tabla-btc tbody")); 
     } 
@@ -79,4 +119,4 @@ const mostrar = ()=>{
      crearElemento(item, document.querySelector(".tabla-btc tbody")); 
     }); 
     agregar(); 
-} 
+   } 
